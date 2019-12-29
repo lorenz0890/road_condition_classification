09cb4cfbc58ea477f385e8b67606544e01ec26ed
Lorenz Kummer <a00947188@unet.univie.ac.at>
2019-12-29 19:16:00 +0100

    updated extraction mp scrimp class

--- pipeline/feature_engineering/feature_extraction/mp_scrimp_extractor.py
+++ pipeline/feature_engineering/feature_extraction/mp_scrimp_extractor.py
@@ -17,16 +17,27 @@ class MPScrimpExtractor(Extractor):
         :param args:
         :return: pandas.DataFrame
         """
-        mp = matrixProfile.scrimp_plus_plus(data, args[0]) #6 or 32
+        mp = matrixProfile.scrimp_plus_plus(data[args[4]].values[:1500], args[0]) #6 or 32
         mtfs, motif_d = motifs.motifs(data, mp, max_motifs=args[1], radius=args[2])  # 2, 23
 
+        return mtfs
+
+    @overrides
+    def select_features(self, data, args=None):
+        """
+        Select features
+        :param data: pandas.DataFrame
+        :param args:
+        :return: pandas.DataFrame
+        """
+        mtfs = data
         sz = len([item for sublist in mtfs for item in sublist]) * args[0]
-        attr_vec = numpy.ndarray(shape=(sz, 3), dtype=float)
-        #print(mtfs)
+        attr_vec = numpy.ndarray(shape=(sz, args[1]), dtype=float) #3
+        # print(mtfs)
         count = 0
         i = 1.0
         for motif in mtfs:
-            for index in motif: #['acceleration_abs', 'road_label']
+            for index in motif:  # ['acceleration_abs', 'road_label']
                 elem = numpy.array(data[args[3]].values[index:index + args[0]])
                 for pos, x in enumerate(elem):
                     attr_vec[count + pos][0] = x[0]
@@ -38,18 +49,8 @@ class MPScrimpExtractor(Extractor):
 
         X = attr_vec.transpose()[:2].transpose()
         y = attr_vec.transpose()[2]
-        #print(X_train.shape)
+        # print(X_train.shape)
         X = pandas.DataFrame(X)
         y = pandas.DataFrame(y)
 
         return X, y
-
-    @overrides
-    def select_features(self, data, args=None):
-        """
-        Select features
-        :param data: pandas.DataFrame
-        :param args:
-        :return: pandas.DataFrame
-        """
-        pass
