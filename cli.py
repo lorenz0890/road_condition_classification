a3e8c3d475f2305ce82dfe25802e6c6f2339799a
Lorenz Kummer <a00947188@unet.univie.ac.at>
2020-01-10 20:02:03 +0100

    working on cli

--- cli.py
+++ cli.py
@@ -1,7 +1,11 @@
 #https://www.sicara.ai/blog/2018-12-18-perfect-command-line-interfaces-python
 #https://pypi.org/project/python-dotenv/
-from sklearn.utils.testing import ignore_warnings
-from sklearn.exceptions import ConvergenceWarning
+import sys
+import warnings
+import os
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+    os.environ["PYTHONWARNINGS"] = "ignore" # Also affect subprocesses
 
 import click
 import json
@@ -51,7 +55,6 @@ def init_pipeline(config):
         pass
     return dao, preprocessor, extractor, model_factory
 
-@ignore_warnings(category=ConvergenceWarning)
 def execute_training(config):
     # 1. Init pipeline
     print('--------------------INIT PIPELINE--------------------')
