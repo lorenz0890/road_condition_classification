05ae5cd013210324cd7e728f25e482a51ad2065c
Lorenz Kummer <a00947188@unet.univie.ac.at>
2020-02-17 12:18:18 +0100

    refactoring

--- pipeline/feature_engineering/feature_extraction/mp_scrimp_extractor.py
+++ pipeline/feature_engineering/feature_extraction/mp_scrimp_extractor.py
@@ -72,9 +72,9 @@ class MPScrimpExtractor(Extractor):
             X = pandas.DataFrame(X)
             print(X.shape)
             if args[1] == 1:
-                X = X.groupby(X.index // int(args[0]/10)).first()
+                X = X.groupby(X.index // int(args[0])).first()
             if args[1] == 2:
-                X = X.groupby(X.index // int(args[0]/10)).mean()
+                X = X.groupby(X.index // int(args[0])).mean()
             print(X.shape)
 
             return X#, y
