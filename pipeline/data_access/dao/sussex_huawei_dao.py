9bd86d2736a3fd9764f81d43cdea72e238037375
Lorenz Kummer <a00947188@unet.univie.ac.at>
2020-02-17 16:57:56 +0100

    refactoring

--- pipeline/data_access/dao/sussex_huawei_dao.py
+++ pipeline/data_access/dao/sussex_huawei_dao.py
@@ -120,9 +120,10 @@ class SussexHuaweiDAO(DAO):
 
                 train = train[0].loc[train[0]['road_label'].isin([1,3])]
                 train = train.loc[train['coarse_label'].isin([5])]
+                if 0.4 < train['road_label'].value_counts()[3]/train.shape[0] < 0.6:
+                    sampling_ok = True
                 print(train['road_label'].value_counts())
-                #print(train_new.count(1)/len(train_new))
-                sampling_ok = True
+
 
 
             if len(all_labels) > 1 or len(all_data) > 1:
