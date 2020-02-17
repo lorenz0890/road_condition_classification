d5398ea55b5e452d2a6e1352c090887688e4f0e2
Lorenz Kummer <a00947188@unet.univie.ac.at>
2020-02-17 15:53:27 +0100

    refactoring

--- pipeline/data_access/dao/sussex_huawei_dao.py
+++ pipeline/data_access/dao/sussex_huawei_dao.py
@@ -113,7 +113,7 @@ class SussexHuaweiDAO(DAO):
                 #print(all_data_labels)
                 all_data, all_labels = zip(*all_data_labels)
 
-                train = numpy.array(all_labels[0:int(0.5*len(all_labels))])
+                train = numpy.ndarray(all_labels[0:int(0.5*len(all_labels))])
                 test =  all_labels[int(0.5 * len(all_labels)):int(0.75 * len(all_labels))]
                 valid = all_labels[int(7.5 * len(all_labels)):]
 
