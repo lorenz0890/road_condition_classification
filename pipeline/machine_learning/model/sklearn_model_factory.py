e33ba211db882a55b9db353e42400bc69d3aa5a8
Lorenz Kummer <a00947188@unet.univie.ac.at>
2020-02-17 21:31:20 +0100

    refactoring

--- pipeline/machine_learning/model/sklearn_model_factory.py
+++ pipeline/machine_learning/model/sklearn_model_factory.py
@@ -33,7 +33,7 @@ class SklearnModelFactory(ModelFactory):
         y_clustering = IsolationForest(behaviour='new',
                                        max_samples='auto',
                                        n_jobs=-1,
-                                       contamination='auto',
+                                       contamination=0.05,
                                        max_features=1.0,
                                        n_estimators=750
                                        ).fit_predict(X)
