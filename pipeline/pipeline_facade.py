6e569f09511c21e3f117487d1e7abad3bf674f9d
Lorenz Kummer <a00947188@unet.univie.ac.at>
2020-02-19 16:47:26 +0100

    ts fresh

--- pipeline/pipeline_facade.py
+++ pipeline/pipeline_facade.py
@@ -110,7 +110,7 @@ class ConcretePipelineFacade(PipelineFacade):
                 id += 1
             test_id = test_id[:data_test.index.size]
             data_test['id'] = test_id
-            
+
             y_train = data_train[['road_label', 'id']].reset_index(drop=True)
             y_train = y_train.groupby(y_train.index // segment_length).agg(lambda x: x.value_counts().index[0]) #majority label in segment
             X_train = data_train[['acceleration_abs', 'id']].reset_index(drop=True)
