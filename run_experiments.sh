497833de5871334b9813260726dc879b22dc8bdc
Lorenz Kummer <lorenz@luke.dm.univie.ac.at>
2020-02-19 18:02:35 +0100

    added run experiments script

---
+++ run_experiments.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "Experiments"
+echo "TSFRESH"
+python cli.py "./config_exp_10hz_15len_euc_tsfresh.json" --training &> exp_10hz_15len_euc_tsfresh.log
+python cli.py "./config_exp_10hz_15len_pca_tsfresh.json" --training &> exp_10hz_15len_pca_tsfresh.log
+python cli.py "./config_exp_10hz_30len_euc_tsfresh.json" --training &> exp_10hz_30len_euc_tsfresh.log
+python cli.py "./config_exp_10hz_30len_pca_tsfresh.json" --training &> exp_10hz_30len_pca_tsfresh.log
+python cli.py "./config_exp_10hz_60len_euc_tsfresh.json" --training &> exp_10hz_60len_euc_tsfresh.log
+python cli.py "./config_exp_10hz_60len_pca_tsfresh.json" --training &> exp_10hz_60len_pca_tsfresh.log
+python cli.py "./config_exp_10hz_90len_euc_tsfresh.json" --training &> exp_10hz_90len_euc_tsfresh.log
+python cli.py "./config_exp_10hz_90len_pca_tsfresh.json" --training &> exp_10hz_90len_pca_tsfresh.log
+python cli.py "./config_exp_10hz_90len_pca_tsfresh.json" --training &> exp_10hz_90len_pca_tsfresh.log
+
+python cli.py "./config_exp_1hz_15len_euc_tsfresh.json" --training &> exp_1hz_15len_euc_tsfresh.log
+python cli.py "./config_exp_1hz_15len_pca_tsfresh.json" --training &> exp_1hz_15len_pca_tsfresh.log
+python cli.py "./config_exp_1hz_30len_euc_tsfresh.json" --training &> exp_1hz_30len_euc_tsfresh.log
+python cli.py "./config_exp_1hz_30len_pca_tsfresh.json" --training &> exp_1hz_30len_pca_tsfresh.log
+python cli.py "./config_exp_1hz_60len_euc_tsfresh.json" --training &> exp_1hz_60len_euc_tsfresh.log
+python cli.py "./config_exp_1hz_60len_pca_tsfresh.json" --training &> exp_1hz_60len_pca_tsfresh.log
+python cli.py "./config_exp_1hz_90len_euc_tsfresh.json" --training &> exp_1hz_90len_euc_tsfresh.log
+python cli.py "./config_exp_1hz_90len_pca_tsfresh.json" --training &> exp_1hz_90len_pca_tsfresh.log
+python cli.py "./config_exp_1hz_90len_pca_tsfresh.json" --training &> exp_1hz_90len_pca_tsfresh.log
+
+echo "SCRIMP_PP"
+python cli.py "./config_exp_10hz_euc_motif.json" --training &> exp_10hz_euc_motif.log
+python cli.py "./config_exp_10hz_pca_motif.json" --training &> exp_10hz_pca_motif.log
+python cli.py "./config_exp_1hz_euc_motif.json" --training &> exp_1hz_euc_motif.log
+python cli.py "./config_exp_1hz_pca_motif.json" --training &> exp_1hz_pca_motif.log
+
